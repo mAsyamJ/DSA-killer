c4072367c204bebef0694c7a5ea2ab7cad02877f
mAsyamJ <jayanegara.asyam@gmail.com>
2025-06-05 22:08:48 +0700

    Java

--- DSA-Diary/Arrays_Hashing/IntToRomawi.java
+++ DSA-Diary/Arrays_Hashing/IntToRomawi.java
@@ -16,17 +16,3 @@ public class IntToRomawi {
 }
 
 // Solution 2
-
-class Solution {
-    public String intToRoman(int num) {
-        String[] thousands = { "", "M", "MM", "MMM" };
-        String[] hundreds = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
-        String[] tens = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
-        String[] units = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
-
-        return thousands[num / 1000] +
-                hundreds[(num % 1000) / 100] +
-                tens[(num % 100) / 10] + // fix: divide by 10 after %100
-                units[num % 10];
-    }
-}
