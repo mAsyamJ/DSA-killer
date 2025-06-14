68787e056232c8fdb89b8bdb03f79d1135a0e307
mAsyamJ <jayanegara.asyam@gmail.com>
2025-06-14 08:34:24 +0700

    my solution

---
+++ DSA-Diary/Arrays_Hashing/Combination3Sum.java
@@ -0,0 +1,47 @@
+// MY SOLUTION
+package Arrays_Hashing;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+// LeetCode Problem: https://leetcode.com/problems/3sum/
+// Problem Statement: Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+// Notice that the solution set must not contain duplicate triplets.
+
+public class Combination3Sum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue; // Skip duplicates
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum < 0) {
+                    left++;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // Skip duplicates for left and right pointers
+                    while (left < right && nums[left] == nums[left + 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right - 1])
+                        right--;
+
+                    left++;
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
